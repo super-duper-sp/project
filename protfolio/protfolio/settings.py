e0d3a5227ad43135ddc4d2f48a027a1763da906c
shubham <shubham15362@gmail.com>
2023-09-06 19:17:17 +0530

    added

--- protfolio/protfolio/settings.py
+++ protfolio/protfolio/settings.py
@@ -143,4 +143,4 @@ MEDIA_ROOT =BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
