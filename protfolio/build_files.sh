44b11e7be30df9e71c5c106266ee87d7f661de1e
shubham <shubham15362@gmail.com>
2023-09-06 17:03:35 +0530

    trying to deployed

--- protfolio/build_files.sh
+++ protfolio/build_files.sh
@@ -4,11 +4,3 @@ python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END"
 
 
-app = application
-
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT  = BASE_DIR / "staticfiles"
-
--- protfolio/protfolio/wsgi.py
+++ protfolio/protfolio/wsgi.py
@@ -14,4 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'protfolio.settings')
 
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+
+app = application
