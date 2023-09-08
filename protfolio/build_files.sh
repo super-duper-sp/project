cd6057ef65d6bb84a23c52c5f694a3c366857ace
shubham <shubham15362@gmail.com>
2023-09-08 10:57:25 +0530

    god setup in build.sh

--- protfolio/build_files.sh
+++ protfolio/build_files.sh
@@ -1,4 +1,11 @@
 # build_files.sh
+
+# Define the path to your virtual environment
+VENV_DIR="god"
+
+# Activate the virtual environment
+source "$VENV_DIR/bin/activate"
+
 pip install -r requirements.txt
 python manage.py collectstatic
 python manage.py makemigrations
