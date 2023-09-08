5937d2131d2e0e380673624e6c730830cf1ffa96
shubham <shubham15362@gmail.com>
2023-09-08 12:56:08 +0530

    remove local media

--- protfolio/protfolio/settings.py
+++ protfolio/protfolio/settings.py
@@ -142,8 +142,8 @@ STATIC_URL = 'static/'
 
 # medias
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
