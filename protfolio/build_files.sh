b365d9c11c10ad03e14196ec6031389c79756874
shubham <shubham15362@gmail.com>
2023-09-08 12:08:04 +0530

    build.sh file changes

--- protfolio/build_files.sh
+++ protfolio/build_files.sh
@@ -1,4 +1,7 @@
 # build_files.sh
+python -m venv venv
+venv\Scripts\activate
+source venv/bin/activate
 pip install -r requirements.txt
 python manage.py collectstatic
 python manage.py makemigrations
