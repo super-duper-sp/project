22cdbd3bcc8b2b847c05fd809f4b347d099a1906
shubham patidar <shubham15362@gmail.com>
2023-10-13 15:29:54 -0700

    done some collect static

--- protfolio/protfolio/settings.py
+++ protfolio/protfolio/settings.py
@@ -148,7 +148,7 @@ MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 
 SUPABASE_API_URL = 'https://hlzyyhtwgggvtjjhllel.supabase.co'
